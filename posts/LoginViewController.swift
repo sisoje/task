d4a49588af503cba1f5a7ed27ff8a9141d704e20
Lazar Otasevic <redhotbits@gmail.com>
2020-03-27 12:36:43 +0100

    added outlets

--- posts/LoginViewController.swift
+++ posts/LoginViewController.swift
@@ -1,8 +1,12 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    @IBOutlet var textFieldUserId: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
+    @IBAction func actionLogin(_ sender: Any) {}
 }
