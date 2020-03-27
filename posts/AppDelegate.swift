632e4f69e99bf13af0bb048182343ab967f46383
Lazar Otasevic <redhotbits@gmail.com>
2020-03-27 12:27:45 +0100

    added swiftformat

--- posts/AppDelegate.swift
+++ posts/AppDelegate.swift
@@ -1,18 +1,7 @@
-//
-//  AppDelegate.swift
-//  posts
-//
-//  Created by Lazar Otašević on 3/27/20.
-//  Copyright © 2020 Lazar Otašević. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -31,7 +20,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
-
--- postsTests/postsTests.swift
+++ postsTests/postsTests.swift
@@ -1,16 +1,7 @@
-//
-//  postsTests.swift
-//  postsTests
-//
-//  Created by Lazar Otašević on 3/27/20.
-//  Copyright © 2020 Lazar Otašević. All rights reserved.
-//
-
-import XCTest
 @testable import posts
+import XCTest
 
 class postsTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -26,9 +17,8 @@ class postsTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
