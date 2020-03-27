cf5393321c6043c7d49b226796e4a2e7c2bd6318
Lazar Otasevic <redhotbits@gmail.com>
2020-03-27 22:34:00 +0100

    preparing ui

--- posts/Source/PostsViewController.swift
+++ posts/Source/PostsViewController.swift
@@ -1,5 +1,20 @@
 import UIKit
 
 final class PostsViewController: UITableViewController {
+    var posts: [Post]!
+
     override func viewDidLoad() {}
+}
+
+// MARK: - tableviewstuff
+
+extension PostsViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        posts.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath)
+        return cell
+    }
 }
