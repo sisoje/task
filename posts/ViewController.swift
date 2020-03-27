08a9a9117023f750dcd9cb33d5e54a8d31fd4838
Lazar Otasevic <redhotbits@gmail.com>
2020-03-27 12:10:22 +0100

    Initial Commit

---
+++ posts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  posts
+//
+//  Created by Lazar Otašević on 3/27/20.
+//  Copyright © 2020 Lazar Otašević. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ postsTests/postsTests.swift
@@ -0,0 +1,34 @@
+//
+//  postsTests.swift
+//  postsTests
+//
+//  Created by Lazar Otašević on 3/27/20.
+//  Copyright © 2020 Lazar Otašević. All rights reserved.
+//
+
+import XCTest
+@testable import posts
+
+class postsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
